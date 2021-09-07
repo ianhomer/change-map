fd59f8753ca953dc6d39091a1c245089b330f528
Ian Homer <ian@purplepip.com>
2021-09-07 21:14:14 +0100

    Lint fix

--- src/engine/draw.ts
+++ src/engine/draw.ts
@@ -1,6 +1,10 @@
 import { Node } from "../types/graph";
 
-export const transform = (xOffset: number, yOffset: number) => (
-  d: Node
-): string =>
-  "translate(" + (xOffset + (d?.x ?? 0)) + "," + (yOffset + (d?.y ?? 0)) + ")";
+export const transform =
+  (xOffset: number, yOffset: number) =>
+  (d: Node): string =>
+    "translate(" +
+    (xOffset + (d?.x ?? 0)) +
+    "," +
+    (yOffset + (d?.y ?? 0)) +
+    ")";
